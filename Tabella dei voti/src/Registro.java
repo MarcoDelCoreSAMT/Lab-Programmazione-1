6a7287acb2f288830adf6aaf5dc0c96213df97b3
MarcoDelCoreCIAO <ibv606@student.edu.ti.ch>
2025-12-03 14:48:11 +0100

    17.10.2025

---
+++ Tabella dei voti/src/Registro.java
@@ -0,0 +1,23 @@
+// 5.2 Tabella dei voti
+// Autore: Marco Del Core
+import java.util.Scanner;
+
+public class Registro {
+    String[] studenti;
+    double[][] voti;
+    Scanner sc = new Scanner(System.in);
+
+    public inserisciVoti() {
+        System.out.print("\nInserisci voto: ");
+
+    }
+    public stampaVoti() {
+
+    }
+    public mediaStudente(int index) {
+
+    }
+    public stampaMedie() {
+
+    }
+}
